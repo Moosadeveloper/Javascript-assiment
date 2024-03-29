5d865004dfca03409bd6587fc3bc26ac4b998220
Moosadeveloper <janmoosa77@gmail.com>
2024-03-29 20:20:48 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+var d = "Ronadlo is the best player in the world ?"
+var r= prompt(d)
+if(r==="yes"){
+   alert("correct ")
+}
+else{
+    alert("wrong ");
+}
+
+var e = "what is your country name ?"
+var f= prompt(e)
+if(f==="pakistan"){
+   alert("correct ")
+}
+else{
+    alert("wrong");
+}
+
+
+var g = "Are you muslim?"
+var h= prompt(g)
+if(h==="yes"){
+   alert("correct ")
+}
+else{
+    alert("wrong");
+}
+
+
+var i = "babaer is best cricketer know ?"
+var j= prompt(i)
+if(j==="no"){
+   alert("correct ")
+}
+else{
+    alert("wrong");
+}
+
+var k = "M.amir is the best bollwer know ?"
+var l= prompt(k)
+if(l==="yes"){
+   alert("correct ")
+}
+else{
+    alert("wrong");
+}
+
+var m = "DO you know who is messi?"
+var n= prompt(m)
+if(n==="yes"){
+   alert("correct ")
+}
+else{
+    alert("wrong");
+}
